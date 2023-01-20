f7381e487123655cd3b609377d4b1ef649745e5f
SashikaMalawana <Sashika.Irosha@gmail.com>
2023-01-20 18:25:51 +0530

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment01Question07
+{
+    public partial class Form1 : Form
+    {
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        Operations oObj = new Operations();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select Image File";
+            ofd.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" + "All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                oObj.LoadOriginalImage(ofd.FileName);
+                string picPath = ofd.FileName.ToString();
+                pictureBox1.ImageLocation = picPath;
+                oObj.LoadGrayScaleImage();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select Image File";
+            ofd.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" + "All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                oObj.LoadOriginalImage(ofd.FileName);
+                string picPath = ofd.FileName.ToString();
+                pictureBox2.ImageLocation = picPath;
+                oObj.LoadBinaryImage();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            oObj.ANDOperation();
+            pictureBox3.ImageLocation = "logicAnd.jpg";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            oObj.OROperation();
+            pictureBox4.ImageLocation = "logicOr.jpg";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            oObj.NotOperation();
+            pictureBox5.ImageLocation = "logicNot.jpg";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            oObj.XorOperation();
+            pictureBox6.ImageLocation = "logicXor.jpg";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select Image File";
+            ofd.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" + "All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                oObj.LoadOriginalImage(ofd.FileName);
+                string picPath = ofd.FileName.ToString();
+                pictureBox7.ImageLocation = picPath;
+                oObj.LoadGrayScaleImage();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select Image File";
+            ofd.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" + "All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                oObj.LoadOriginalImage(ofd.FileName);
+                string picPath = ofd.FileName.ToString();
+                pictureBox8.ImageLocation = picPath;
+                oObj.LoadBinaryImage();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            String stringOpacity = comboBox1.Text.ToString();
+            float floatOpacity = float.Parse(stringOpacity);
+
+            Bitmap first = new Bitmap(pictureBox7.Image);
+            Bitmap second = oObj.SetImageOpacity(pictureBox8.Image, floatOpacity);
+            Bitmap result = new Bitmap(Math.Max(first.Width, second.Width), Math.Max(first.Height, second.Height));
+            Console.WriteLine(first.Width);
+            Graphics g = Graphics.FromImage(result);
+            g.DrawImageUnscaled(first, 0, 0);
+            g.DrawImageUnscaled(second, 0, 0);
+            pictureBox9.Image = result;
+            result.Save("result.jpg");
+            pictureBox9.ImageLocation = "result.jpg";
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select Image File";
+            ofd.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" + "All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                oObj.LoadOriginalImage(ofd.FileName);
+                string picPath = ofd.FileName.ToString();
+                pictureBox10.ImageLocation = picPath;
+                oObj.LoadGrayScaleImage();
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select Image File";
+            ofd.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" + "All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                oObj.LoadOriginalImage(ofd.FileName);
+                string picPath = ofd.FileName.ToString();
+                pictureBox11.ImageLocation = picPath;
+                oObj.LoadBinaryImage();
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            oObj.ANDOperation();
+            pictureBox12.ImageLocation = "logicAnd.jpg";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("0.3");
+            comboBox1.Items.Add("0.7");
+        }
+    }
+}
---
+++ Operations.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace Assignment01Question07
+{
+    class Operations
+    {
+        IplImage src, grayImage, maskImage, binaryImage, grayImgResult, colorImage, colorMask, negativeImage;
+
+        public void LoadOriginalImage(String fname)
+        {
+            src = Cv.LoadImage(fname, LoadMode.Color);
+            Cv.SaveImage(fname, src);
+        }
+
+        public void LoadGrayScaleImage()
+        {
+            grayImage = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+            Cv.CvtColor(src, grayImage, ColorConversion.RgbToGray);
+            Cv.SaveImage("gray.jpg", grayImage);
+
+            colorImage = Cv.CreateImage(src.Size, BitDepth.U8, 3);
+            Cv.CvtColor(src, colorImage, ColorConversion.RgbaToRgb);
+        }
+
+        public void LoadGrayScaleMaskImage()
+        {
+            maskImage = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+            Cv.CvtColor(src, maskImage, ColorConversion.RgbaToGray);
+            Cv.SaveImage("mask.jpg", maskImage);
+
+            colorMask = Cv.CreateImage(src.Size, BitDepth.U8, 3);
+            Cv.CvtColor(src, colorMask, ColorConversion.RgbaToRgb);
+        }
+
+        public void LoadBinaryImage()
+        {
+            LoadGrayScaleMaskImage();
+            binaryImage = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+            Cv.Threshold(maskImage, binaryImage, 150, 255, ThresholdType.Binary);
+            Cv.SaveImage("binary.jpg", binaryImage);
+
+        }
+
+        public void PerformANDLogicOperations()
+        {
+            grayImgResult = Cv.CreateImage(grayImage.Size, BitDepth.U8, 1);
+
+            for (int y = 1; y < binaryImage.Height; y++)
+            {
+                for (int x = 1; x < binaryImage.Width; x++)
+                {
+                    double getPixelValue = 0, newPixelValue = 0, value = 0;
+
+                    getPixelValue = Cv.GetReal2D(binaryImage, y, x);
+                    value = Cv.GetReal2D(grayImage, y, x);
+
+                    if (getPixelValue == 0)
+                    {
+                        newPixelValue = 0;
+                    }
+                    else if (getPixelValue == 255)
+                    {
+                        newPixelValue = value;
+                    }
+
+                    Cv.SetReal2D(grayImgResult, y, x, newPixelValue);
+                }
+            }
+            Cv.SaveImage("logicAnd.jpg", grayImgResult);
+        }
+
+        public void PerformORLogicOperations()
+        {
+            grayImgResult = Cv.CreateImage(grayImage.Size, BitDepth.U8, 1);
+
+            for (int y = 1; y < binaryImage.Height; y++)
+            {
+                for (int x = 1; x < binaryImage.Width; x++)
+                {
+                    double getPixelValue = 0, newPixelValue = 0, value = 0;
+
+                    getPixelValue = Cv.GetReal2D(binaryImage, y, x);
+                    value = Cv.GetReal2D(grayImage, y, x);
+
+                    if (getPixelValue == 0)
+                    {
+                        newPixelValue = value;
+                    }
+                    else if (getPixelValue == 255)
+                    {
+                        newPixelValue = value+50;
+                    }
+
+                    Cv.SetReal2D(grayImgResult, y, x, newPixelValue);
+                }
+            }
+            Cv.SaveImage("logicOr.jpg", grayImgResult);
+        }
+
+        public void ANDOperation()
+        {
+            grayImgResult = Cv.CreateImage(colorImage.Size, BitDepth.U8, 3);
+            Cv.And(colorImage, colorMask, grayImgResult);
+            Cv.SaveImage("logicAnd.jpg", grayImgResult);
+        }
+
+        public void OROperation()
+        {
+            grayImgResult = Cv.CreateImage(colorImage.Size, BitDepth.U8, 3);
+            Cv.Or(colorImage, colorMask, grayImgResult);
+            Cv.SaveImage("logicOr.jpg", grayImgResult);
+        }
+
+        public void NotOperation()
+        {
+            grayImgResult = Cv.CreateImage(colorImage.Size, BitDepth.U8, 3);
+            Cv.Not(colorImage, grayImgResult);
+            Cv.SaveImage("logicNot.jpg", grayImgResult);
+        }
+
+        public void XorOperation()
+        {
+            grayImgResult = Cv.CreateImage(colorImage.Size, BitDepth.U8, 3);
+            Cv.Xor(colorImage, colorMask, grayImgResult);
+            Cv.SaveImage("logicXor.jpg", grayImgResult);
+        }
+
+        public Bitmap SetImageOpacity(Image image, float opacity)
+        {
+            try
+            {
+                Bitmap bmp = new Bitmap(image.Width, image.Height);
+
+                using (Graphics gfx = Graphics.FromImage(bmp))
+                {
+                    ColorMatrix matrix = new ColorMatrix();
+                    matrix.Matrix33 = opacity;
+                    ImageAttributes attributes = new ImageAttributes();
+                    attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+                    gfx.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+                }
+                return bmp;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+}
